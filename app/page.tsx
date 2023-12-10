3013bb8f7849abc31f388c89337926fea0041b6a
Halyna Harasymovych <halyna.harasymovych@tallium.com>
2023-12-10 17:17:24 +0200

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  console.log('scs', process.env.NEXT_PUBLIC_API_KEY);
+
   return (
     <main className={styles.main}>
       Hello!
--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -22,6 +22,8 @@ const WelcomePage = () => {
     }
   };
 
+ 
+  
   return (
     <div> 
       <form> 
